184586673ef4a174ae54ec0003a03f89501c76c8
eluinstra <eluinstra@luin.dev>
2021-08-28 20:50:59 +0200

    split REST API per service cleanup

--- src/main/java/dev/luin/file/client/web/GBWebConfig.java
+++ src/main/java/dev/luin/file/client/web/GBWebConfig.java
@@ -15,30 +15,16 @@
  */
 package dev.luin.file.client.web;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import javax.xml.ws.Endpoint;
-import javax.xml.ws.soap.SOAPBinding;
 
-import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.endpoint.Server;
-import org.apache.cxf.ext.logging.LoggingFeature;
-import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
-import org.apache.cxf.jaxrs.openapi.OpenApiFeature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import dev.luin.digikoppeling.gb.client.service.GBService;
 import dev.luin.digikoppeling.gb.client.service.GBServiceImpl;
-import dev.luin.file.client.core.service.FileService;
-import dev.luin.file.client.core.service.FileServiceImpl;
-import io.vavr.Tuple;
-import io.vavr.collection.HashMap;
-import io.vavr.collection.Map;
 import lombok.AccessLevel;
-import lombok.val;
 import lombok.experimental.FieldDefaults;
 
 @Configuration
@@ -46,17 +32,7 @@ import lombok.experimental.FieldDefaults;
 public class GBWebConfig extends WebConfig
 {
 	@Autowired
-	FileService fileService;
-	@Autowired
 	GBService gbService;
-
-	@Bean
-	public Endpoint fileServiceEndpoint()
-	{
-		val result = publishEndpoint(fileService,"/file","http://luin.dev/file/client/1.0","FileService","FileServicePort");
-		((SOAPBinding)result.getBinding()).setMTOMEnabled(true);
-		return result;
-	}
 
 	@Bean
 	public Endpoint gbServiceEndpoint()
@@ -64,35 +40,9 @@ public class GBWebConfig extends WebConfig
 		return publishEndpoint(gbService,"/gb","http://luin.dev/digikoppeling/client/gb/1.0","GBService","GBServicePort");
 	}
 
-	@Bean(name="cxf")
-	public SpringBus springBus()
-	{
-		val result = new SpringBus();
-		val f = new LoggingFeature();
-		f.setPrettyLogging(true);
-		result.setFeatures(Collections.singletonList(f));
-		return result;
-	}
-
 	@Bean
-	public Server createJAXRSServer()
+	public Server createGBJAXRSServer()
 	{
-		val sf = new JAXRSServerFactoryBean();
-		sf.setBus(springBus());
-		sf.setAddress("/rest/v1");
-		sf.setProvider(createJacksonJsonProvider());
-		sf.setFeatures(Arrays.asList(new OpenApiFeature()));
-		registerResources(sf);
-		registerBindingFactory(sf);
-		return sf.create();
+		return createJAXRSServer(GBServiceImpl.class,gbService,"/gb");
 	}
-
-	protected Map<Class<?>,Object> getResourceClasses()
-	{
-		val result = HashMap.<Class<?>,Object>ofEntries(
-			Tuple.of(FileServiceImpl.class, fileService),
-			Tuple.of(GBServiceImpl.class, gbService));
-		return result;
-	}
-
 }
